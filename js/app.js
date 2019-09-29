16a347b48c120a4f018c815fe2656172a5367ef5
akash-yoobee <akash.yoobee@gmail.com>
2019-09-29 21:51:24 +1300

    j

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// Everything in this file is other people's work, changed slightly to make my page work. None of it is my original work.
-
-$(document).ready(function() {
-  $(".carousel").carousel({ dist: 0 });
-  $(".modal").modal({ dismissible: false });
-  $(".collapsible").collapsible();
-  $(".fixed-action-btn").floatingActionButton({ toolbarEnabled: true });
-});
---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Everything in this file is other people's work, changed slightly to make my page work. None of it is my original work.
+
+$(document).ready(function() {
+  $(".carousel").carousel({ dist: 0 });
+  $(".modal").modal({ dismissible: false });
+  $(".collapsible").collapsible();
+  $(".fixed-action-btn").floatingActionButton({ toolbarEnabled: true });
+});
