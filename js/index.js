f8358b8783fab08af763f48bcddb0f6da83a96ff
akash-yoobee <akash.yoobee@gmail.com>
2019-10-20 13:35:44 +1300

    20 Oct from home

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-// Everything in this file is other people's work, changed slightly to make my page work. None of it is my original work.
-
 $(document).ready(function() {
   $(".carousel").carousel({ dist: 0 });
   $(".modal").modal({ dismissible: false });
